6d128cc0521ec4b0a16389c4548ac236c7383192
Uttam Dhakar <uttamdhakar1234@gmail.com>
2025-10-16 16:23:22 +0530

    Add Port 8000

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const productRoutes = require("./routes/productRoutes");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
